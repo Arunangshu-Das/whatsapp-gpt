5f6337a71aeeb0f81197a816df076b0f155c3b88
Aruangshu Das <darunangshu2002@gmail.com>
2023-04-14 23:37:32 +0530

    second commit

--- index.js
+++ index.js
@@ -2,35 +2,49 @@ const { Client } =require('whatsapp-web.js')
 const qrcode= require('qrcode-terminal')
 const {Configuration,OpenAIApi }=require('openai')
 require('dotenv').config()
+const express = require("express");
+const app=express();
 
-const client=new Client();
+async function hello(){
 
-client.on('qr',(qr)=>{
-    qrcode.generate(qr,{small:true})
-})
+    const client=new Client();
 
-client.on('ready',()=>{
-    console.log('client ready')
-})
+    client.on('qr',(qr)=>{
+        qrcode.generate(qr,{small:true})
+    })
 
-client.initialize();
+    client.on('ready',()=>{
+        console.log('client ready')
+    })
 
-const configuration=new Configuration({
-    apiKey:process.env.SECRET_KEY,
-})
+    client.initialize();
 
-const openai=new OpenAIApi(configuration)
+    const configuration=new Configuration({
+        apiKey:process.env.SECRET_KEY,
+    })
 
-async function runcompile(message){
-    const completion=await openai.createCompletion({
-        model:"text-davinci-003",
-        prompt: message,
-        max_tokens: 500,
-    });
-    return completion.data.choices[0].text;
+    const openai=new OpenAIApi(configuration)
+
+    async function runcompile(message){
+        const completion=await openai.createCompletion({
+            model:"text-davinci-003",
+            prompt: message,
+            max_tokens: 500,
+        });
+        return completion.data.choices[0].text;
+    }
+
+    client.on('message',message => {
+        console.log(message.body);
+        runcompile(message.body).then(result => message.reply(result));
+    })
 }
 
-client.on('message',message => {
-    console.log(message.body);
-    runcompile(message.body).then(result => message.reply(result));
+app.get("/",(req,res)=>{
+    hello();
+    res.send('ok')
+})
+
+app.listen(8000,(err)=>{
+    console.log(err)
 })
