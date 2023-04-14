87b59b6ba68f3cc0082b0beb3869531b1a47d088
Arunangshu Das <86423568+Arunangshu-Das@users.noreply.github.com>
2023-04-15 00:33:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-const { Client } =require('whatsapp-web.js')
-const qrcode= require('qrcode-terminal')
-const {Configuration,OpenAIApi }=require('openai')
-require('dotenv').config()
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
 const express = require("express");
-const app=express();
+const app = express();
 
-const client=new Client();
+const client = new Client();
 
-client.on('qr',(qr)=>{
-    qrcode.generate(qr,{small:true})
-})
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
 
-client.on('ready',()=>{
-    console.log('client ready')
-})
+client.on('ready', () => {
+    console.log('client ready');
+    hello();
+});
 
 client.initialize();
 
-const configuration=new Configuration({
-    apiKey:process.env.SECRET_KEY,
-})
+const configuration = new Configuration({
+    apiKey: process.env.SECRET_KEY,
+});
 
-const openai=new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
-async function hello(){
-
-    async function runcompile(message){
-        const completion=await openai.createCompletion({
-            model:"text-davinci-003",
-            prompt: message,
-            max_tokens: 500,
-        });
-        return completion.data.choices[0].text;
-    }
-
-    client.on('message',message => {
-        console.log(message.body);
-        runcompile(message.body).then(result => message.reply(result));
-    })
+async function runcompile(message) {
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: message,
+        max_tokens: 500,
+    });
+    return completion.data.choices[0].text;
 }
 
-app.get("/",(req,res)=>{
-    hello();
-    res.send('ok')
-})
+function hello() {
+    client.on('message', message => {
+        console.log(message.body);
+        runcompile(message.body).then(result => message.reply(result));
+    });
+}
 
-app.listen(8000,(err)=>{
-    console.log(err)
-})
+app.get("/", (req, res) => {
+    res.send('ok');
+});
+
+app.listen(8080, (err) => {
+    console.log(err);
+});
